99fbd05df92dea6c43ac174784b5d487844aaf07
wang <953147580@qq.com>
2017-07-21 14:32:28 +0800

    新增登陆路由

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var login = require('./routes/login');
+var login = require('./routes/login');//登陆路由
 var app = express();
 
 // view engine setup
